815180c7555593747182dddff56a4b18696942f0
eshwar863 <eshwarladda863@gmail.com>
2024-05-21 17:38:08 +0530

    Implemented JWT

--- src/main/java/com/TaskManagement/Config/JwtAuthenticationFilter.java
+++ src/main/java/com/TaskManagement/Config/JwtAuthenticationFilter.java
@@ -20,10 +20,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final UserService userService;
 
-    public JwtAuthenticationFilter(JwtService jwtService, UserService userService) {
-        this.jwtService = jwtService;
-        this.userService = userService;
-    }
 
 
     @Override
